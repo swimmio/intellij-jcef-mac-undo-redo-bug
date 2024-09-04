55daa379d549ca7d4dd9aa19c2fb20463904bad9
Segev Finer <segev@swimm.io>
2024-09-04 15:31:23 +0300

    Fix

--- src/main/kotlin/com/github/segevfiner/intellijjcefmacundoredobug/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/segevfiner/intellijjcefmacundoredobug/toolWindow/MyToolWindowFactory.kt
@@ -1,7 +1,5 @@
 package com.github.segevfiner.intellijjcefmacundoredobug.toolWindow
 
-import com.github.segevfiner.intellijjcefmacundoredobug.services.MyProjectService
-import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
