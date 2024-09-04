8cb325f61baef07847a9184caf46fa81874a4645
GitHub Action <action@github.com>
2024-09-04 11:36:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/segevfiner/intellijjcefmacundoredobug/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/segevfiner/intellijjcefmacundoredobug/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.segevfiner.intellijjcefmacundoredobug.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.segevfiner.intellijjcefmacundoredobug.MyBundle
+import com.github.segevfiner.intellijjcefmacundoredobug.services.MyProjectService
 import javax.swing.JButton
 
 
